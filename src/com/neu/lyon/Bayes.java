8a3245bb467ae584cfe12a9f2e4b252ec9fdaa90
Lyon-NEU <wanlplab@gmail.com>
2014-06-25 22:40:25 +0800

    First commit

---
+++ src/com/neu/lyon/Bayes.java
@@ -0,0 +1,22 @@
+package com.neu.lyon;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A simple implement of Bayes
+ * @author Lyon
+ * @date 2014/06/25
+ */
+public class Bayes {
+	private Set<String>stopword;
+	public Bayes()
+	{
+		stopword=new HashSet<String>();
+	}
+	public void train()
+	{
+		
+	}
+	
+}
