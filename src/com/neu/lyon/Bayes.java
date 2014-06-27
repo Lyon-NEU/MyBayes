f12049a4893c10fd1117f82cb0b4c715e2e42e0a
Lyon-NEU <wanlplab@gmail.com>
2014-06-27 23:32:19 +0800

    ci

--- src/com/neu/lyon/Bayes.java
+++ src/com/neu/lyon/Bayes.java
@@ -5,8 +5,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -17,13 +19,35 @@ import java.util.Set;
 public class Bayes {
 	private Set<String>stopword;
 	private List<String>categories;
+	private Map<String, Integer>wordCount=null;
 	public Bayes()
 	{
 		stopword=new HashSet<String>();
 		categories=new ArrayList<String>();
+		wordCount=new HashMap<String,Integer>();
 	}
-	public void train()
+	/**
+	 * training model
+	 * @param trainingDir
+	 * @param category
+	 */
+	public void train(String trainingDir,String category)throws Exception
 	{
+		if (trainingDir.isEmpty()) {
+			System.out.println("directory is null");
+			return;
+		}
+		String currentDir=trainingDir+category;
+		File dir=new File(currentDir);
+		File[]files=dir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			File f=files[i];
+			BufferedReader in=new BufferedReader(new FileReader(f));
+			String line;
+			while ((line=in.readLine())!=null) {
+				
+			}
+		}
 		
 	}
 	/**
@@ -53,7 +77,7 @@ public class Bayes {
 		File[]files=dir.listFiles();
 		for(int i=0;i<files.length;i++){
 			if(files[i].isDirectory()){
-				//categories.add(files[i]);
+				categories.add(files[i].getName());
 			}
 		}
 	}
